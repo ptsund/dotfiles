2bee3bcf974e6416d194a6499970ebf61079566e
Patrik Sundström <patrik.sundstrom@gmail.com>
2022-02-06 11:31:52 +0100

    Update dmwebsearch.sh

--- scripts/dmwebsearch.sh
+++ scripts/dmwebsearch.sh
@@ -9,7 +9,7 @@ declare -A engines
 engines[google]='https://google.com/search?q='
 engines[duckduckgo]='https://duckduckgo.com/?q='
 
-engine=$(printf '%s\n' "${!engines[@]}" | sort | ${dmenu} 'ﰍ' "$@")
+engine=$(printf '%s\n' "${!engines[@]}" | sort | ${dmenu} 'Engine:' "$@")
 url="${engines["${engine}"]}"
 query=$(echo "$engine" | ${dmenu} 'Query:')
 query="$(echo "${query}" | jq -s -R -r @uri)"
