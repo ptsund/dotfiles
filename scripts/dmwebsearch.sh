be5ef144e264a6503c0a5e41d43940468b79204e
Patrik Sundström <patrik.sundstrom@gmail.com>
2022-02-06 11:29:58 +0100

    Add dmwebsearch.sh

---
+++ scripts/dmwebsearch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+dmenu='dmenu -i -l 20 -p'
+browser='qutebrowser'
+
+declare -A engines
+engines[google]='https://google.com/search?q='
+engines[duckduckgo]='https://duckduckgo.com/?q='
+
+engine=$(printf '%s\n' "${!engines[@]}" | sort | ${dmenu} 'ﰍ' "$@")
+url="${engines["${engine}"]}"
+query=$(echo "$engine" | ${dmenu} 'Query:')
+query="$(echo "${query}" | jq -s -R -r @uri)"
+${browser} "${url}${query}"
+
